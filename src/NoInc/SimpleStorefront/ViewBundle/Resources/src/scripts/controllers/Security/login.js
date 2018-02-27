cc56c0b29b2f70e12b52c4a0de51acde75a04944
vincentpurcell <vincent@noinc.com>
2018-02-27 10:17:57 -0500

    JS Linter fixes

--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/Security/login.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/controllers/Security/login.js
@@ -1,11 +1,11 @@
 app.controller('loginController', ($scope, $http) => {
     $scope.user = {
         username: null,
-        password: null
+        password: null,
     };
 
     $scope.login = () => $http.post('/login', {
         _username: $scope.user.username.toLowerCase().trim(),
-        _password: $scope.user.password.trim()
-    }).then(() => window.location = '/app/recipes');
+        _password: $scope.user.password.trim(),
+    }).then(window.location.reload());
 });
--- src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/factories/recipeProvider.js
+++ src/NoInc/SimpleStorefront/ViewBundle/Resources/src/scripts/factories/recipeProvider.js
@@ -1,12 +1,8 @@
-app.factory('recipeProvider', ($http) => {
-    const getRecipes = () => {
-        return $http.get('/api/recipes')
-            .then((response) => {
-                return response.data;
-            });
-    };
+app.factory('recipeProvider', ['$http', ($http) => {
+    const getRecipes = $http.get('/api/recipes')
+        .then(response => response.data);
 
     return {
         getRecipes,
     };
-});
+}]);
